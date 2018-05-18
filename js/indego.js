f0fe61036c39cdc4f32217d0d91726e9ce564ed4
Stuzfuz <github@studener.at>
2018-05-18 16:05:30 +0200

    Complete new UI design

--- js/indego.js
+++ js/indego.js
@@ -6,56 +6,120 @@ function handleError(status, error) {
         console.log(status + " -> " + error);
         console.log(localStorage.getItem("contextId"));
     } else {
-        //logout();
-        $("#login").hide();
-        $("#loading").hide();
-        $("#view").hide();
+        $("#error").html("Username or password wrong!");
         $("#error").show();
     }
 }
 
 function connect() {                   
-	  console.log("connect");  	  
-	  user = $("#user").val();
-	  pass = $("#password").val();
+    console.log("connect");  	  
+    user = $("#user").val();
+    pass = $("#password").val();
 
-	  if (user == "" || pass == "") {
-	  	alert("User or pass not set");
-	  	return false;
-	  }
+    if (user == "" || pass == "") {
+    $("#error").html("Username or password not set!");
+    $("#error").show();
 
- 	  
-	  $.ajax({          
-		  url: URL + "authenticate",
-          timeout: TIMEOUT, 
-		  type: "POST",  mimeType: "application/json",
-		  contentType: "application/json",
-		  dataType: "json",		  
-          data: JSON.stringify({ device:'', os_type:'Android', os_version:'6.0', dvc_manuf:'unknown', dvc_type:'unknown' }),		  
-		  beforeSend: function( xhr ) {
-            xhr.setRequestHeader ("Authorization", "Basic " + btoa(user + ":" + pass));
-			$("#login").hide();
-			$("#loading").show();
-          },
-          error: function(xhr, status, error){
-		      handleError(status, error);
-          },
-          success: function(result){    						
-			console.log("connected");
-			console.log(result);
-			
-			localStorage.setItem("contextId", result['contextId']);
-			localStorage.setItem("almSn", result['alm_sn']);
-			
-			$("#loading").hide();
-			$("#view").show();
-			getState();
-          }
-		});      
+    return false;
+    }
+
+    
+    $.ajax({          
+        url: URL + "authenticate",
+        timeout: TIMEOUT, 
+        type: "POST",  mimeType: "application/json",
+        contentType: "application/json",
+        dataType: "json",		  
+        data: JSON.stringify({ device:'', os_type:'Android', os_version:'6.0', dvc_manuf:'unknown', dvc_type:'unknown' }),		  
+        beforeSend: function( xhr ) {
+        xhr.setRequestHeader ("Authorization", "Basic " + btoa(user + ":" + pass));
+        $("#login").hide();
+        $("#loading").show();
+        },
+        error: function(xhr, status, error){
+            handleError(status, error);
+        },
+        success: function(result){    						
+        console.log("connected");
+        console.log(result);
+        
+        localStorage.setItem("contextId", result['contextId']);
+        localStorage.setItem("almSn", result['alm_sn']);
+        sessionStorage.setItem("loggedIn", "true");
+        
+        location.href="index.html"
+        }
+    });      
 };
 
+function updateTopBanners() {
+    setAlerts();
+    setState();
+}
 
-function getAlerts(){
+function showDashboard() {
+    $("#content").html("");
+    showMap();
+    updateTopBanners();
+}
+
+function showLoadingImage() {
+    $("#content").html("");
+    $("#content-loader").show();
+}
+
+function hideLoadingImage() {
+    $("#content-loader").hide();
+}
+
+function setPercentageMowed(percent_mowed) {
+    $("#percent_mowed").html(percent_mowed + "% mowed!");
+}
+
+function getDropdownAlertsTemplate(id, headline, message) {
+    return "<div class=\"dropdown-divider\"></div><span class=\"dropdown-item\" href=\"#\"><span class=\"text-danger\"><strong><i class=\"fa fa-fw\"></i>" + headline + " (" + id + ")!</strong></span><div class=\"dropdown-message small\">" + message + "</div></span>";
+}
+
+function setActiveErrors(errors, count) {
+    $("#active_errors").html(count + " Alert(s)!");
+    if(count > 0) {
+        $("#alerts-indicator").addClass("text-warning");
+    }
+
+    html = "";
+    $.each(errors, function(index, value) {
+        html += getDropdownAlertsTemplate(value.alert_id, value.headline, value.message);
+    });
+    $("#dropdown-alerts .dropdown-menu").html("<h6 class=\"dropdown-header\">Alerts:</h6>" + html + "<div class=\"dropdown-divider\"></div><a class=\"dropdown-item small\" href=\"#\" onclick=\"showAlerts();\">View all alerts</a>");
+}
+
+function setAlerts() {
+    $.ajax({          
+        url: URL + "alerts",
+        timeout: TIMEOUT, 
+        type: "GET", 
+        contentType: "application/json",
+        dataType: "json",		  
+        beforeSend: function( xhr ) {
+            xhr.setRequestHeader ("x-im-context-id", localStorage.getItem("contextId"));
+        },
+        error: function(xhr, status, error){
+            handleError(status, error);
+        },
+        success: function(result) {
+
+            if(typeof(result) == "undefined" || result.length == 0) {
+                count = 0;
+            } else {
+                count = result.length;
+            }
+            setActiveErrors(result, count);
+        }
+    });  
+}
+
+function showAlerts(){
+    showLoadingImage();
 	  $.ajax({          
 		  url: URL + "alerts",
           timeout: TIMEOUT, 
@@ -75,20 +139,23 @@ function getAlerts(){
           	console.log(result[0]);
 
 			if(typeof(result) == "undefined" || result.length == 0) {
-			    html = "Keine Fehler gefunden";
+			    html = "No errors found!";
             } else {
 			    html = "";
 			    $.each(result, function(index, value) {
-			        html += "<b>" + value.headline + "</b><br />" + value.message + "<br /><a href='#' onclick='deleteAlert(\"" + value.alert_id + "\")'>L&ouml;schen</a><br /><br />";
+			        html += "<b>" + value.headline + "</b><br />" + value.message + "<br /><a href='#' onclick='deleteAlert(\"" + value.alert_id + "\")'>Delete</a><br /><br />";
                 });
             }
-			$("#output").html(html);
+            hideLoadingImage();
+			$("#content").html(html);
           }
-		});      
+        });   
+        updateTopBanners();   
 };
 
-function getCalender() {
-    $("#output").html("Funktion noch nicht implementiert!");
+function showCalendar() {
+    $("#content").html("Not implemented yet!");
+    updateTopBanners();
     /*$.ajax({
         url: URL + "alms/" +  localStorage.getItem("almSn") + "/predictive/calendar",
         timeout: TIMEOUT,
@@ -121,38 +188,74 @@ function getCalender() {
     });*/
 }
 
-function getMachineState(){
-	  $.ajax({          
-	     url: URL + "alms/" +  localStorage.getItem("almSn"),
-          timeout: TIMEOUT, 
-		  type: "GET",
-		  contentType: "application/json",
-		  dataType: "json",		  		  
-		  beforeSend: function( xhr ) {
+/*function getMachineState(){
+    $.ajax({          
+        url: URL + "alms/" +  localStorage.getItem("almSn"),
+        timeout: TIMEOUT, 
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",		  		  
+        beforeSend: function( xhr ) {
+        xhr.setRequestHeader ("x-im-context-id", localStorage.getItem("contextId"));
+            $("#loading").show();
+            $("#view").hide();
+        },
+        error: function(xhr, status, error){
+            handleError(status, error);
+        },
+        success: function(result){
+
+            $("#loading").hide();
+            $("#view").show();
+            html = result.alm_mode + "<br />";
+            html += result.alm_firmware_version + "<br />";
+            html += result.alm_name + "<br />";
+            html += result.needs_service + "<br />";
+
+            html += "<br />";
+
+            $("#output").html(html);
+        }
+    });        
+};*/
+
+function setState(longpoll){
+    var laststate = 0;
+    console.log("getState");
+    $.ajax({
+        url: URL + "alms/" +  localStorage.getItem("almSn") + "/state" + ( (longpoll==true) ? "?longpoll=true&timeout=600&last=" + laststate : ""),
+        //timeout: TIMEOUT,
+        timeout: 1000 * 700,
+        type: "GET",
+        contentType: "application/json",
+        dataType: "json",
+        beforeSend: function( xhr ) {
             xhr.setRequestHeader ("x-im-context-id", localStorage.getItem("contextId"));
-              $("#loading").show();
-              $("#view").hide();
-          },
-          error: function(xhr, status, error){
-	         handleError(status, error);
-          },
-          success: function(result){
+        },
+        success: function(result){
+            console.log(result);
 
-			  $("#loading").hide();
-			  $("#view").show();
-			  html = result.alm_mode + "<br />";
-			  html += result.alm_firmware_version + "<br />";
-			  html += result.alm_name + "<br />";
-			  html += result.needs_service + "<br />";
+            switch (parseInt(result["state"])){
+                case 519: state = "Mower standing/stopped"; break;
+                case 257: state = "Loading"; break;
+                case 514: state = "Locating"; break;
+                case 776: state = "Driving back to Dockingstation"; break;
+                case 518: state = "Mowing"; break;
+                case 517: state = "Pause"; break;
+                case 513: state = "Mowing"; break;
+                case 263: state = "Docked"; break;
+                case 774: state = "Driving back to Dockingstation"; break;
+                case 258: state = "Docked"; break;
+                default: state = "defaultstate";;
+            }
+            $("#status").html("<b>State: </b>" + state);
 
-			  html += "<br />";
+            setPercentageMowed(result["mowed"]);
+        }
+    });
+}
 
-			  $("#output").html(html);
-          }
-		});        
-};
-
-function getState(longpoll){
+/*function getState(longpoll){
     var laststate = 0;
     console.log("getState");
     $.ajax({
@@ -189,7 +292,7 @@ function getState(longpoll){
         	$('#output').html(html);
         }
     });
-};
+};*/
 
 function toDate(dateTime) {
     var from = dateTime.substr(0, 10).split("-");
@@ -211,12 +314,13 @@ function deleteAlert(idx){
         },
         success: function(result){
             console.log('deleted');
-            getAlerts();
+            showAlerts();
         }
     });
 }
 
-function getWeather() {
+function showWeather() {
+    showLoadingImage();
     $.ajax({
         url: URL + "alms/" +  localStorage.getItem("almSn") + "/predictive/weather",
         timeout: TIMEOUT,
@@ -251,40 +355,34 @@ function getWeather() {
 				}
                 html += outputdate + " -> " + datetime.substr(11, 2) + " Uhr -> " + "Regenwahrscheinlichkeit: " +  weather.LocationWeather.forecast.intervals[i].prrr + " -> " + "Grad: " +  weather.LocationWeather.forecast.intervals[i].tt + ": " + weather.LocationWeather.forecast.intervals[i].wwtext + "<br />";
 			}
-
-			$('#output').html("<h3>Wettervorhersage für " + weather.LocationWeather.location.country + ", " + weather.LocationWeather.location.name + "</h3>" + html + "</div>");
+            hideLoadingImage();
+			$('#content').html("<h3>Weatherforecast for " + weather.LocationWeather.location.country + ", " + weather.LocationWeather.location.name + "</h3>" + html + "</div>");
         }
     });
+    updateTopBanners();
 }
 
-function isAlreadyLoggedIn() {
-	if (localStorage.getItem("contextId")) {
-		$("#loading").hide();
-		$("#login").hide();
-	    $("#view").show();
-        getState(true);
-	}
-}
-
-function getMap(){   
-	  $.ajax({          
-		  url: URL + "alms/" +  localStorage.getItem("almSn") + "/map",
-          timeout: TIMEOUT,   
-		  type: "GET",  mimeType: "application/html",          
-		  beforeSend: function( xhr ) {
-            xhr.setRequestHeader ("x-im-context-id", localStorage.getItem("contextId"));
-			$("#loading").show();			
-          },
-          error: function(xhr, status, error){
-		      handleError(status, error);
-          },
-          success: function(result){  
-			$("#loading").hide();
-			map = result;
-			console.log(result);
-			$('#output').html("<div style='width: 50%; margin: 0 auto'>" + map + "</div>");
-          }
-		});      
+function showMap(){  
+    showLoadingImage();
+    $.ajax({          
+        url: URL + "alms/" +  localStorage.getItem("almSn") + "/map",
+        timeout: TIMEOUT,   
+        type: "GET",  mimeType: "application/html",          
+        beforeSend: function( xhr ) {
+        xhr.setRequestHeader ("x-im-context-id", localStorage.getItem("contextId"));
+        $("#loading").show();			
+        },
+        error: function(xhr, status, error){
+            handleError(status, error);
+        },
+        success: function(result){  
+        $("#loading").hide();
+        map = result;
+        console.log(result);
+        hideLoadingImage();
+        $('#content').html("<div style='width: 100%; margin: 0 auto'>" + map + "</div>");
+        }
+    });      
 };
 
 function home(){
@@ -351,10 +449,13 @@ function pause() {
 }
 
 function logout() {
-	localStorage.removeItem("contextId");
-	location.reload();
+    localStorage.removeItem("contextId");
+    sessionStorage.removeItem("loggedIn");
+	location.href = "login.html";
 }
 
-$('#view').ready(function() {
-	isAlreadyLoggedIn();
+$('#page-top').ready(function() {
+    if(sessionStorage.getItem("loggedIn") == "true" && localStorage.getItem("contextId")) {
+        showDashboard();
+    }
 });
